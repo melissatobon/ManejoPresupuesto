afe18e4f7e12d6e3b4d5192bfbf35b8e6594b343
merestrepo <merestrepo@DESKTOP-TFAA3KQ>
2022-10-26 13:37:33 -0500

    Add project files.

---
+++ ManejoPresupuesto/Servicios/RepositorioCuentas.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioCuentas
+    {
+        Task<IEnumerable<Cuenta>> Buscar(int usuarioId);
+        Task Crear(Cuenta cuenta);
+    }
+    public class RepositorioCuentas: IRepositorioCuentas
+    {
+        private readonly string connectionString;
+
+        public RepositorioCuentas(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(Cuenta cuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(@"INSERT INTO Cuentas (Nombre, TipoCuentaId, Descripcion, Balance)
+                                                                        VALUES (@Nombre, @TipoCuentaId, @Descripcion, @Balance);
+
+                                                                            SELECT SCOPE_IDENTITY();", cuenta);
+
+            cuenta.Id = id;
+        }
+
+        public async Task<IEnumerable<Cuenta>> Buscar(int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<Cuenta>(@"select Cuentas.Id,	Cuentas.Nombre, Balance, tc.Nombre as TipoCuenta
+                                                                                from Cuentas
+                                                                                inner join TiposCuentas tc
+                                                                                on tc.Id=Cuentas.TipoCuentaId
+                                                                                where tc.UsuarioId= @UsuarioId
+                                                                                order by tc.Orden", new { usuarioId });
+        }
+    }
+}
